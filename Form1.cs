2d277f881672bd006f6ea934511ff38d5e315586
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-09-10 16:19:28 +0700

    Add feature to save and clear Twitter API Credential

--- Form1.cs
+++ Form1.cs
@@ -7,19 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TweetAutomationProject.Business;
 
 namespace TweetAutomationProject {
   public partial class TweetAutomationFrom : Form {
+    private CredentialSetting credentialSetting = new CredentialSetting();
     public TweetAutomationFrom() {
       InitializeComponent();
+      ReloadCredential();
     }
 
+    private void ReloadCredential() {
+      ConsumerKey.Text = credentialSetting.ConsumerKeySetting;
+      ConsumerSecret.Text = credentialSetting.ConsumerSecretSetting;
+      AccessTokenKey.Text = credentialSetting.AccessTokenKeySetting;
+      AccessTokenSecret.Text = credentialSetting.AccessTokenSecretSetting;
+    }
     private void button_save(object sender, EventArgs e) {
-
+      credentialSetting.ConsumerKeySetting = ConsumerKey.Text;
+      credentialSetting.ConsumerSecretSetting = ConsumerSecret.Text;
+      credentialSetting.AccessTokenKeySetting = AccessTokenKey.Text;
+      credentialSetting.AccessTokenSecretSetting = AccessTokenSecret.Text;
+      credentialSetting.Save();
     }
 
     private void button_clear(object sender, EventArgs e) {
-
+      ConsumerKey.Clear();
+      ConsumerSecret.Clear();
+      AccessTokenKey.Clear();
+      AccessTokenSecret.Clear();
+      credentialSetting.Reset();
     }
 
     private void button_send(object sender, EventArgs e) {
