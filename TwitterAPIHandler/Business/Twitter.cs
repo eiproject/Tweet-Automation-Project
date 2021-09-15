f639a40e293ec7ec8b4b884071c9f417003e34d7
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-09-15 10:07:38 +0700

    change directory

---
+++ TwitterAPIHandler/Business/Twitter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Net;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+
+namespace TwitterAPIHandler.Business {
+  public class Twitter {
+    private HttpClient _client;
+    public Twitter() {
+      _client = new HttpClient();
+    }
+
+    public void PostTweet() {
+      Task.Run(() => MainAsync());
+    }
+
+    private async Task MainAsync(string ock, string ocs, string ot, string ots) {
+      using (var client = new HttpClient()) {
+        client.BaseAddress = new Uri("https://api.twitter.com");
+        client.DefaultRequestHeaders.Add("oauth_consumer_key", ock);
+        client.DefaultRequestHeaders.Add("oauth_consumer_secret", ocs);
+        client.DefaultRequestHeaders.Add("oauth_token", ot);
+        client.DefaultRequestHeaders.Add("oauth_token_secret", ots);
+        client.DefaultRequestHeaders.Accept.Add
+          (new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
+        var content = new FormUrlEncodedContent(new[]
+        {
+                new KeyValuePair<string, string>("status", "Twitter post using C#. Ignore this.")
+            });
+        var result = await client.PostAsync("/1.1/statuses/update.json", content);
+        string resultContent = await result.Content.ReadAsStringAsync();
+        Console.WriteLine(resultContent);
+      }
+    }
+  }
+}
---
+++ TwitterAPIHandler/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TwitterAPIHandler.Business;
+
+namespace TwitterAPIHandler {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Twitter API Handler Project!");
+      Twitter twt = new Twitter();
+      twt.PostTweet();
+      Console.ReadLine();
+    }
+  }
+}
